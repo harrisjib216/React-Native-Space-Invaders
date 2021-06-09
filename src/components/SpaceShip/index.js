1cd9605a71a7ca9e44885ba8524a9b3b596c4745
harrisjib216 <harrisjib216@gmail.com>
2021-06-09 00:12:04 -0400

    spaceship can move but there is a better solution

--- src/components/SpaceShip/index.js
+++ src/components/SpaceShip/index.js
@@ -1,14 +1,44 @@
-import React from 'react';
-import { StyleSheet, View, Text, Animated } from 'react-native';
+import React, { useState } from 'react';
+import {
+	StyleSheet,
+	View,
+	Text,
+	Animated,
+	PanResponder,
+	Responder,
+} from 'react-native';
+import { screenWidth } from '../../modules/helpers';
 
 const SpaceShip = ({ ...props }) => {
+	const [position, setPosition] = useState(
+		new Animated.ValueXY({ x: screenWidth / 2 - 25, y: 10 }),
+	);
+
+	const panResponder = React.useRef(
+		PanResponder.create({
+			onStartShouldSetPanResponder: (evt, gestureState) => true,
+			onMoveShouldSetPanResponder: (evt, gestureState) => true,
+			onPanResponderGrant: (env, gestureState) => {
+				position.x.setValue(gestureState.x0 - 25);
+			},
+			onPanResponderMove: (evt, gestureState) => {
+				if (gestureState.moveX <= 30) {
+				} else if (gestureState.moveX >= screenWidth - 30) {
+				} else {
+					position.x.setValue(gestureState.moveX - 25);
+				}
+			},
+		}),
+	).current;
+
 	return (
 		<View style={styles.container}>
 			<Animated.Image
+				{...panResponder.panHandlers}
 				source={{
 					uri: 'https://cdn0.iconfinder.com/data/icons/video-games-outline/60/050_-_Space_Invaders-512.png',
 				}}
-				style={styles.image}
+				style={[styles.image, position.getLayout()]}
 			/>
 		</View>
 	);
@@ -26,8 +56,10 @@ const styles = StyleSheet.create({
 		backgroundColor: 'white',
 	},
 	image: {
+		top: 15,
 		width: 50,
 		height: 50,
+		position: 'absolute',
 	},
 });
 
